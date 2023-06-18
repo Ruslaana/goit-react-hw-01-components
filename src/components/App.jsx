fa278cee8b47be8cf75535674485be04ee7234c5
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-06-18 13:00:45 +0200

    task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import Profile from './Profile/Profile.jsx';
 import Statistics from './Data/Data.jsx';
 import FriendList from './Friends/FriendList.jsx';
-// import Transactions from './Transactions/Transactions.jsx'
+import TransactionHistory from './Transactions/Transactions.jsx';
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
-// import transactions from 'transactions.json'
+import transactions from 'transactions.json';
 
 export const App = () => {
   return (
@@ -19,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
